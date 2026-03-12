b18f7c2d31125c61e44e70270bac6d32e1399c73
5000dll <1394319770@qq.com>
2026-03-12 21:37:26 +0800

    开始界面制作完成，对应美术素材上传

--- Assets/Scripts/StartCanvas.cs
+++ Assets/Scripts/StartCanvas.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class StartCanvas : MonoBehaviour
 {
+    [Header("太阳神鸟")]
+    public RectTransform rotatingImage1;   // 要旋转的 UI 图片
+    public RectTransform rotatingImage2;   // 要旋转的 UI 图片
+    public float rotateSpeed = 60f;       // 每秒旋转角度（度/秒）
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,12 @@ public class StartCanvas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (rotatingImage1 != null && rotatingImage2 != null)
+        {
+            // UI 图像围绕自身圆心顺时针绕 Z 轴持续旋转
+            rotatingImage1.Rotate(0f, 0f, -rotateSpeed * Time.deltaTime);
+            rotatingImage2.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+        }
     }
 
     public void CloseCanvas()
